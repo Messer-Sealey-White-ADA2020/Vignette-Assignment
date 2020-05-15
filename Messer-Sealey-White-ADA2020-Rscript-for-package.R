f7ff4b21745c45a2766d24d04be2308a276bb3b3
Britt A White <24brittwhite@gmail.com>
2020-05-14 20:38:13 -0400

    igraph with viper A

--- Messer-Sealey-White-ADA2020-Rscript-for-package.R
+++ Messer-Sealey-White-ADA2020-Rscript-for-package.R
@@ -1,6 +1,16 @@
-library(devtools)
-library(roxygen2)
-library(withr)
-library(tidyverse)
-library(usethis)
-# make sure that you have  
+library(igraph)
+
+v <- read.csv("vipers.csv")
+
+edges1 <- (v$A)
+nodes1 <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+target <- c("A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A")
+
+nte <- cbind(nodes1, target, edges1)
+
+id <- c(1:26)
+
+idnodes <- cbind(nodes1, id)
+
+v <- graph_from_data_frame(d=nte, vertices=idnodes, directed=FALSE)
+plot(v)
