a129780ecb46bde84faf840d8dbea7dcae48f328
Britt A White <24brittwhite@gmail.com>
2020-05-16 12:55:46 -0400

    Intial igraph code for both treatments - not yet using self created package with dependencies on igraph

--- Messer-Sealey-White-ADA2020-Rscript-for-package.R
+++ Messer-Sealey-White-ADA2020-Rscript-for-package.R
@@ -2,15 +2,27 @@ library(igraph)
 
 v <- read.csv("vipers_hook.csv")
 
-edges1 <- (v$A)
+weight <- (v$A)
 nodes1 <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 target <- c("A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A")
 
-nte <- cbind(nodes1, target, edges1)
+nte <- cbind(nodes1, target, weight)
+head(nte)
 
 id <- c(1:26)
 
 idnodes <- cbind(nodes1, id)
+head(idnodes)
 
-v <- graph_from_data_frame(d=nte, vertices=idnodes, directed=FALSE)
-plot(v)
+v4plot <- graph_from_data_frame(d=nte, vertices=idnodes, directed=FALSE)
+plot(v4plot)
+
+library(ggraph)
+
+cos_mat <- cosine_matrix(dat, lower = .01, upper = .80, filt = .80)
+
+set.seed(1839)
+ggraph(v, layout = "nicely") +
+  geom_edge_link(aes(alpha = weight), show.legend = FALSE) + 
+  geom_node_label(aes(label = name)) +
+  theme_void()
