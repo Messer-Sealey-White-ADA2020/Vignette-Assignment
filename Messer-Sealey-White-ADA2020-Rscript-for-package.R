ff5c9e85db40bd88f099436d700d42808e90e06c
Britt A White <24brittwhite@gmail.com>
2020-05-17 15:01:48 -0400

    updated work

--- Messer-Sealey-White-ADA2020-Rscript-for-package.R
+++ Messer-Sealey-White-ADA2020-Rscript-for-package.R
@@ -1,28 +1,170 @@
 library(igraph)
+library(tidyverse) # to enable tidying of data
+library(reshape2) # to use with melt function to convert matrix to objects igraph can plot
+library(visNetwork) 
+aerial <- read_csv("blacktreatments_heads2020f.csv", col_names=TRUE)
 
-v <- read.csv("vipers_hook.csv")
+colnames(aerial) <-  c("names", 1:length(b0))
+cleanaerial <- select(aerial, -names)
+head(cleanaerial)
 
-weight <- (v$A)
-nodes1 <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
-target <- c("A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A")
+cleanaerial <- as.matrix(cleanaerial)
+cleanaerial[lower.tri(cleanaerial, diag=TRUE)] <- NA
+meltaerial <- setNames(melt(cleanaerial), c('ind1', 'ind2', 'values'))
+head(meltaerial)
 
-nte <- cbind(nodes1, target, weight)
-head(nte)
+meltaerial <- filter(meltaerial, !is.na(values))
+summary(meltaerial)
 
-id <- c(1:26)
+aerialplot <- graph_from_data_frame(meltaerial, directed = FALSE, vertices = NULL)
+plot(aerialplot, vertex.label = NA)
 
-idnodes <- cbind(nodes1, id)
-head(idnodes)
+visIgraph(aerialplot, randomSeed = TRUE) %>% 
+  visOptions(manipulation = TRUE)
 
-v4plot <- graph_from_data_frame(d=nte, vertices=idnodes, directed=FALSE)
-plot(v4plot)
+VisNPlot <- function(x,
+                     nodes = data$nodes, 
+                     edges = data$edges, 
+                     height = "500px"){
 
-library(ggraph)
+x = toVisNetworkData %>% 
+  graph_from_data_frame(meltaerial, directed = FALSE, vertices = NULL) %>%
+filter(meltaerial, !is.na(values))%>%
+setNames(melt(cleanaerial), c('ind1', 'ind2', 'values')) 
+colnames(aerial) <-  c("names", 1:length(aerial)) %>%
+      cleanaerial <- as.matrix(cleanaerial) %>%
+      cleanaerial[lower.tri(cleanaerial, diag=TRUE)] <- NA %>%
+      cleanaerial <- select(x, -names) 
+}
 
-cos_mat <- cosine_matrix(dat, lower = .01, upper = .80, filt = .80)
+VisNPlot(aerial)
 
-set.seed(1839)
-ggraph(v, layout = "nicely") +
-  geom_edge_link(aes(alpha = weight), show.legend = FALSE) + 
-  geom_node_label(aes(label = name)) +
-  theme_void()
+cv_summary <- function(d, country_list = "World",
+                       plot = TRUE, facet = "country",
+                       status = c("confirmed", "deaths", "recovered")) {
+  
+  # based on `wes_palettes()` color schemes GrandBudapest1, IsleofDogs1,
+  # IsleofDogs2 from the {wesanderson} package
+  my_palette <- c(
+    "#5B1A18", "#FD6467", "#F1BB7B", "#D67236",
+    "#0F0D0E", "#9986A5", "#79402E", "#CCBA72", "#D9D0D3", "#8D8680",
+    "#EAD3BF", "#AA9486", "#B6854D", "#39312F", "#1C1718"
+  )
+  
+  if (facet == "country") {
+    fill <- "variable"
+    n <- length(unique(d$variable)) / 2
+    # need only half of unique # of variable (3)
+  }
+  
+  if (facet == "variable") {
+    fill <- "country"
+    n <- length(country_list)
+    # need number of countries
+  }
+  
+  if ("All" %in% country_list) {
+    country_list <- unique(d$country)
+    country_list <- setdiff(country_list, "World")
+  }
+  
+  if ("World" %in% country_list) {
+    d <- d %>% filter(country %in% country_list)
+    
+    totals <- d %>%
+      group_by(variable) %>%
+      summarize(
+        country = "World",
+        cases = max(cases),
+        population = max(population),
+        area = max(area),
+        density = max(density),
+        rate = max(rate, na.rm = TRUE),
+        on = max(date)
+      ) %>%
+      select(country, variable, cases, population, area, density, rate, on) %>%
+      arrange(variable) %>%
+      ungroup()
+  }
+  
+  if ("World" %nin% country_list) {
+    d <- d %>% filter(country %in% country_list)
+    totals <- d %>%
+      group_by(country, variable) %>%
+      summarize(
+        cases = max(cases),
+        population = max(population),
+        area = max(area),
+        density = max(density),
+        rate = max(rate, na.rm = TRUE),
+        on = max(date),
+        gdp_capita = fmode(gdp_capita),
+        income = fmode(income),
+        life_expectancy = fmode(life_expectancy),
+        max_sd = max(soc_dist),
+        max_mr = max(mov_rest)
+      ) %>%
+      select(
+        country, variable, cases, population, area, density, rate,
+        gdp_capita, income, life_expectancy, max_sd, max_mr, on
+      ) %>%
+      arrange(country, variable) %>%
+      ungroup()
+  }
+  
+  if (plot == TRUE) {
+    cc <- filter(d, variable %in% status)
+    cum_cases_plot <- ggplot(
+      data = cc,
+      # use the tidy evaluation pronoun .data to slice the chosen fill
+      # variable from the data frame
+      aes(
+        x = date, y = cases + 1, color = .data[[fill]],
+        fill = .data[[fill]]
+      )
+    ) +
+      geom_point(size = 0.5) +
+      geom_line() +
+      # use the tidy evaluation pronoun .data to slice the chosen facet_wrap
+      # variable from the data frame
+      facet_wrap(~ .data[[facet]], ncol = 5) +
+      xlab("Date") +
+      ylab("Log Cumulative Cases") +
+      scale_y_log10(
+        breaks = trans_breaks("log10", function(x) 10^x),
+        labels = trans_format("log10", math_format(10^.x))
+      ) +
+      scale_color_manual(
+        aesthetics = c("color", "fill"),
+        name = NULL, values = my_palette
+      )
+    
+    dc <- filter(d, variable %in% paste0("daily_", status))
+    daily_cases_plot <- ggplot(
+      data = dc,
+      aes(
+        x = date, y = cases, color = .data[[fill]],
+        fill = .data[[fill]]
+      )
+    ) +
+      geom_point(size = 0.5) +
+      geom_line() +
+      facet_wrap(~ .data[[facet]], ncol = 5) +
+      xlab("Date") +
+      ylab("Daily Cases") +
+      scale_color_manual(
+        aesthetics = c("color", "fill"),
+        name = NULL, values = my_palette
+      )
+  }
+  
+  if (plot == TRUE) {
+    return(list(
+      totals = totals,
+      cum_cases_plot = cum_cases_plot,
+      daily_cases_plot = daily_cases_plot
+    ))
+  } else {
+    return(list(totals = totals))
+  }
+}
