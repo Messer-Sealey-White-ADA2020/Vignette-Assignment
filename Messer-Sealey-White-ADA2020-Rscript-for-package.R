9e332dbc04543a43a910502026afb93e118214f5
Britt A White <24brittwhite@gmail.com>
2020-05-18 15:55:00 -0400

    edits

--- Messer-Sealey-White-ADA2020-Rscript-for-package.R
+++ Messer-Sealey-White-ADA2020-Rscript-for-package.R
@@ -6,7 +6,7 @@ library(visNetwork)
 x <- read_csv("blacktreatments_heads2020f.csv", col_names=TRUE)
 
 ### Tidying Data for analysis  
-melting data matrix for aerial treatment
+#melting data matrix for aerial treatment
 colnames(aerial) <-  c("names", 1:length(aerial))
 cleanaerial <- select(aerial, -names)
 head(cleanaerial)
@@ -35,3 +35,35 @@ x[lower.tri(x, diag=TRUE)] %>% NA
 setNames(melt(x), c('ind1', 'ind2', 'values'))
 filter(x, !is.na(values))
 
+
+### PLaying around with qgraph package
+library(qgraph)
+library(reshape2)
+
+Monkeys <- read_csv("EM_monkey.csv", col_names=TRUE)
+Sname <- c(Monkeys$name)
+Sno <- c(1:length(Monkeys))
+Source <- cbind(Sname,Sno)
+colnames(Monkeys) <-  c("name", 1:length(Monkeys))
+Monkeys <- select(Monkeys, -name)
+Monkeys <- as.matrix(Monkeys)
+Monkeys[lower.tri(Monkeys, diag=TRUE)] <- NA 
+Monkeys <- setNames(melt(Monkeys), c('Source', 'Target', 'Weight'))
+Monkeys <- filter(Monkeys, !is.na(Weight))
+summary(Monkeys)
+qgraph(Monkeys, directed = FALSE, layout = "spring")
+
+Source
+
+aerial <- read_csv("blacktreatments_heads2020f.csv", col_names=TRUE)
+Sname <- c(aerial$names)
+Sno <- c(1:length(aerial))
+Source <- cbind(Sname,Sno)
+colnames(aerial) <-  c("name", 1:length(aerial))
+aerial <- select(aerial, -name)
+aerial <- as.matrix(aerial)
+aerial[lower.tri(aerial, diag=TRUE)] <- NA 
+aerial <- setNames(melt(aerial), c('Source', 'Target', 'Weight'))
+aerial <- filter(aerial, !is.na(Weight))
+summary(aerial)
+qgraph(aerial, directed = FALSE, layout = "spring")
