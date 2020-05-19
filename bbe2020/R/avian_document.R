a2b7f7f6f5de9d0bb6270602f8aa663787d420d9
Briana-16 <60192704+Briana-16@users.noreply.github.com>
2020-05-18 22:46:52 -0400

    commit temp package
    commit temp package

---
+++ bbe2020/R/avian_document.R
@@ -0,0 +1,11 @@
+#' Avian treatment matrix
+#'
+#' Antipredator behavior similarities for the avian treatment dataset
+#'
+#' @format A data frame with 319 rows and 1 variable:
+#' \describe{
+#'   \item{individuals}{similarities, calcualted from PSI}
+#'   ...
+#' }
+#' @source \url{https://github.com/Messer-Sealey-White-ADA2020/Vignette-Assignment/blob/master/blacktreatments_heads2020f.csv}
+"avian"
